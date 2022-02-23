507e891e0a7476a4737248a98ecb9324dc7c562b
Eduardo Visconti <duduhsg@gmail.com>
2022-02-23 14:43:46 -0600

    aula 03

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('Olá mundo');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); //Variável constante/fixa.
+
+let contador = 0; //Variável que pode ser alterada.
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    
+    contador = contador + 1;
+
+    console.log(contador);
+}
